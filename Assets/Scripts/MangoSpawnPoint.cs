6ec900ec5528bf0e4977aee0deb4e8d74f963fbe
Sonny Williams <sonnywilliams@hotmail.co.uk>
2020-08-22 19:48:53 +0100

    Initial Commit
    Art & Template Code

---
+++ Assets/Scripts/MangoSpawnPoint.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class MangoSpawnPoint : MonoBehaviour
+{
+    public GameObject mango;
+
+    public void SpawnMango()
+    {
+       Instantiate(mango, transform.position, Quaternion.identity);
+    }
+
+}
